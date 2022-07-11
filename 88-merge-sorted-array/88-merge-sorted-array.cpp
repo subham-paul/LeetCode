f825a2a231efc78196d70fae46a3ca94784f1b80
Subham Paul <52645265+subham-paul@users.noreply.github.com>
2022-07-11 08:46:43 +0530

    Time: 4 ms (53.02%), Space: 9 MB (71.80%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+            int left = m - 1, right = n - 1;
+    for (int i = nums1.size() - 1; i >= 0; i--)
+    {
+        if (left >= 0 && right >= 0)
+        {
+            if (nums1[left] > nums2[right])
+            {
+                nums1[i] = nums1[left];
+                left--;
+            }
+            else
+            {
+                nums1[i] = nums2[right];
+                right--;
+            }
+        }
+        else if (left >= 0)
+        {
+            nums1[i] = nums1[left];
+            left--;
+        }
+        else
+        {
+            nums1[i] = nums2[right];
+            right--;
+        }
+    }
+    }
+};
