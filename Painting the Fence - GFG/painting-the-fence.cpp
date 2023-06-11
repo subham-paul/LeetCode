b2259a90f01ab3ef064c84c52cf14c517eb74efe
Subham Paul <52645265+subham-paul@users.noreply.github.com>
2023-06-11 12:13:12 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -7,18 +7,22 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    // SOLVE USING TABULATION METHOD
     long long mod = 1000000007;
     long long countWays(int n, int k){
-    vector<long long> dp(n + 1, 0);
-    dp[1] = k;
-    dp[2] = (k + k * (k - 1));
-
-    for (int i = 3; i <= n; i++) {
-
-        dp[i] =((dp[i-2]+ dp[i-1] ) *(k-1) ) % mod;
-    }
-    return dp[n];
+        if(n==1) return k;
+        // SPACE OPTIMIZATION
+        long long prev2 = k;
+        long long prev1 = (k + k * (k - 1));
+    
+        for (int i = 3; i <= n; i++) {
+    
+            long long curr = ((prev2 + prev1) * (k - 1)) % mod;
+    
+            // shhift -> yaha hi galti karunga ya karungi
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
 
